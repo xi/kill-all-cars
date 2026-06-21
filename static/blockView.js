21b668cd579e5d2c820c9ca171044a58f2016a5b
Tobias Bengfort <tobias.bengfort@posteo.de>
2026-06-21 15:18:45 +0200

    revert some blockview

--- static/blockView.js
+++ static/blockView.js
@@ -1,12 +1,11 @@
 const FACTOR = 0.4;
-const PADDING = 20;
 
 export class BlockView {
     constructor(block, ctx) {
         this.block = block;
         this.ctx = ctx;
 
-        this.size = Math.min(ctx.canvas.width, ctx.canvas.height / FACTOR) / (block.constructor.size + 2) / 2;
+        this.size = Math.min(ctx.canvas.width, ctx.canvas.height / FACTOR) / block.constructor.size / 2;
         this.x0 = ctx.canvas.width / 2 - this.size * block.constructor.size;
         this.y0 = ctx.canvas.height / 2;
     }
@@ -28,13 +27,6 @@ export class BlockView {
     }
 
     render() {
-        this.ctx.fillStyle = 'currentColor';
-        this.ctx.font = '16px sans-serif';
-        this.ctx.fillText(`residential space: ${this.block.residential}`, (this.ctx.canvas.width - 2 * PADDING) / 4 * 0 + PADDING, PADDING);
-        this.ctx.fillText(`commercial served: ${this.block.commercialServed}`, (this.ctx.canvas.width - 2 * PADDING) / 4 * 1 + PADDING, PADDING);
-        this.ctx.fillText(`green served: ${this.block.greenServed}`, (this.ctx.canvas.width - 2 * PADDING) / 4 * 2 + PADDING, PADDING);
-        this.ctx.fillText(`pollution: ${this.block.pollution}`, (this.ctx.canvas.width - 2 * PADDING) / 4 * 3 + PADDING, PADDING);
-
         for (let y = 0; y < this.block.constructor.size; y++) {
             for (let x = this.block.constructor.size - 1; x >= 0; x--) {
                 this.ctx.beginPath();
@@ -59,7 +51,7 @@ export class BlockView {
     handleClick(event, state) {
         const size = this.block.constructor.size;
         const [x, y] = this.fromScreenXY(event.clientX, event.clientY);
-        if (x < -2 || y < -2 || x >= size + 2 || y >= size + 2) {
+        if (x < 0 || y < 0 || x >= size || y >= size) {
             state.block = null;
             return true;
         }
